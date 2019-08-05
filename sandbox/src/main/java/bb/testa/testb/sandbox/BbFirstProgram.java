3a792cc139319e77958293901f7d4e51a3801159
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-08-06 00:50:48 +0200

    squarle added

--- sandbox/src/main/java/bb/testa/testb/sandbox/BbFirstProgram.java
+++ sandbox/src/main/java/bb/testa/testb/sandbox/BbFirstProgram.java
@@ -3,7 +3,12 @@ package bb.testa.testb.sandbox;
 public class BbFirstProgram {
 	
   public static void main(String[] args) {
-    System.out.println("Hello world !!!");
+    String somebody = "world";
+    System.out.println("Hello world !!!"+ somebody);
+
+    double l = 8.0;
+    double s = l*l;
+    System.out.println("powierzchni kwadratu o boku " + l + " jest równa " + s);
   }
   
 }
