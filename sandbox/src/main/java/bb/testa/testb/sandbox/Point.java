baad1c39e5a9ec88c9edc2ad143f546da370e9db
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-08-14 18:50:23 +0200

    Point Fixes according to review

--- sandbox/src/main/java/bb/testa/testb/sandbox/Point.java
+++ sandbox/src/main/java/bb/testa/testb/sandbox/Point.java
@@ -11,13 +11,14 @@ public class Point {
         this.y = y;
     }
 
-    public static double distance(Point p1, Point p2) {
+    public double distance(Point p2) {
+
         double odl;
         double odlx = 0;
         double odly = 0;
 
-        odlx = p1.x - p2.x;
-        odly = p1.y - p2.y;
+        odlx = this.x - p2.x;
+        odly = this.y - p2.y;
         odl = Math.sqrt((odlx * odlx)+(odly * odly));
 
         return odl;
@@ -27,10 +28,11 @@ public class Point {
     public static void main(String[] args) {
         Point p1 = new Point(1, 1);
         Point p2 = new Point(4, 5);
-        calculation calc = new calculation(p1,p2);
-        calc.distanceMethod();
-        System.out.println("The distance between point p1 ( " + p1.x + " , " + p1.y  + " ) and point p2 ( " + p2.x + " , "+p2.y + " ) equals " + Point.distance(p1, p2));
-        System.out.println("The distance between this points, calculated from method, also equals " + calc.distanceMethod());
+        p1.distance(p2);
+        //calculation calc = new calculation(p1,p2);
+        //calc.distanceMethod();
+        //System.out.println("The distance between point p1 ( " + p1.x + " , " + p1.y  + " ) and point p2 ( " + p2.x + " , "+p2.y + " ) equals " + Point.distance(p1, p2));
+        System.out.println("The distance between this points, calculated from method, also equals " + p1.distance(p2));
 
     }
 }
