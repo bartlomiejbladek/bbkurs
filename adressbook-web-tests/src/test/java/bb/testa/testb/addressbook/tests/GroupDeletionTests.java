9406a9b85b5e60a6edef4bf483b57ff0c20c44fa
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-08-19 21:15:26 +0200

    precondition for group tests

--- adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package bb.testa.testb.addressbook.tests;
 
+import bb.testa.testb.addressbook.model.GroupData;
 import org.testng.annotations.*;
 
 
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
