319b99aa583808e2b1a59cf9f409cf886563134d
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-08-25 21:19:20 +0200

    Groups Lambda comparation

--- adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/tests/AddGroupsTests.java
+++ adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/tests/AddGroupsTests.java
@@ -2,8 +2,10 @@ package bb.testa.testb.addressbook.tests;
 
 import bb.testa.testb.addressbook.model.GroupData;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class AddGroupsTests extends TestBase {
@@ -12,10 +14,16 @@ public class AddGroupsTests extends TestBase {
   public void testAddGroups() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
+    GroupData group = new GroupData("test3b", "test2", null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size() , before.size() +1);
 
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
