c54373ee77d4bb463fe725fd576bc135f71b2ee1
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-09-02 15:58:35 +0200

    Fixes after review

--- adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/appmanager/ContactHelper.java
+++ adressbook-web-tests/src/test/java/bb/testa/testb/addressbook/appmanager/ContactHelper.java
@@ -79,8 +79,8 @@ public class ContactHelper extends HelperBase {
         List<ContactData> contacts = new ArrayList<ContactData>();
         List<WebElement> elements = wd.findElements(By.xpath("//tr[contains(@name,'entry')]"));
         for (WebElement element : elements) {
-            String lastname = element.findElement(By.xpath("//tr[contains(@name,'entry')]/td[2]")).getText();
-            String firstname = element.findElement(By.xpath("//tr[contains(@name,'entry')]/td[3]")).getText();
+            String lastname = element.findElement(By.xpath(".//td[2]")).getText();
+            String firstname = element.findElement(By.xpath(".//td[3]")).getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
             ContactData contact = new ContactData(id, firstname, lastname, null, null, null, null);
             contacts.add(contact);
