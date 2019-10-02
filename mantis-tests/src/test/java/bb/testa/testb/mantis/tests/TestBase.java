5eaafa83cfb506fb2e8d8a2bb94d8aef1e57540d
bartlomiejbladek <bartlomiejbladek@gmail.com>
2019-10-02 20:41:09 +0200

    registration test MantisBt

--- mantis-tests/src/test/java/bb/testa/testb/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/bb/testa/testb/mantis/tests/TestBase.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+
 public class TestBase {
 
 
@@ -15,11 +17,13 @@ public class TestBase {
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
 
     public void tearDown() throws Exception {
+        app.ftp().restore("config_inc.php", "config_inc.php.bak");
         app.stop();
     }
 
